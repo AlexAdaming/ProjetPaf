ae41563119950d99c8e8a4684e8941ec4671c375
Gander <victor.gander@gmail.com>
2019-06-21 10:44:45 +0200

    daffaire developpement

--- src/app/site-avocat/d-affaire/d-affaire.component.ts
+++ src/app/site-avocat/d-affaire/d-affaire.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { AffaireService } from './../../services/affaire.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./d-affaire.component.css']
 })
 export class DAffaireComponent implements OnInit {
+  dAffaireForm: FormGroup;
+  affaireActif;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private affaireService: AffaireService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.dAffaireForm = this.formBuilder.group({
+      reference: ['', Validators.required],
+      titre: ['', Validators.required],
+      description: ['', Validators.required],
+      status: ['', Validators.required],
+    });
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.affaireService.getAffaire(id).subscribe((data => {
+      this.affaireActif = data;
+    }));
   }
-
+  // editAffaire(index) {
+  //   this.dAffaireForm =
+  //   this.affaireService.editMode = true;
+  // }
 }
